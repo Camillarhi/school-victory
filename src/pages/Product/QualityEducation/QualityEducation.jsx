c16aff7347e379eabeca60a90638d4d0a0bed81a
Camillarhi <rhitanene@gmail.com>
2023-09-04 23:20:12 +0100

    made it responsive

--- src/pages/Product/QualityEducation/QualityEducation.jsx
+++ src/pages/Product/QualityEducation/QualityEducation.jsx
@@ -12,7 +12,7 @@ import UnderLine from "../../../components/UnderLine/UnderLine";
 
 export default function QualityEducation() {
   return (
-    <div className=" md:w-[65.625rem] h-[45.1593rem] py-[10rem] flex justify-between items-center flex-col md:flex-row mt-7 md:mt-0">
+    <div className=" md:w-[65.625rem] md:h-[45.1593rem] py-[10rem] flex justify-between items-center flex-col md:flex-row mt-7 md:mt-0">
       <div className=" md:w-1/2 w-full h-[25.1593rem] flex justify-center items-center relative overflow-hidden">
         <div className=" bg-[#DDF4FF] rounded-full md:w-[60%] md:h-[75%] w-[80%] h-[70%] absolute"></div>
         <img src={Girl} alt="girl" className=" absolute " />
@@ -40,7 +40,7 @@ export default function QualityEducation() {
           className=" absolute md:-bottom-[4.0906rem] -bottom-[2.7906rem] right-[3.2931rem] md:w-[13.6878rem] md:h-[12.7706rem] w-[8.0628rem] h-[7.1456rem]"
         />
       </div>
-      {/* <div className=" w-[23.8125rem] h-[18.5rem] flex flex-col gap-y-[2.1875rem]">
+      <div className=" md:w-[23.8125rem] md:h-[18.5rem] flex flex-col md:gap-y-[2.1875rem] gap-y-5 mt-7">
         <UnderLine />{" "}
         <Title text={"Get Quality Education"} className=" w-[15.1875rem]" />
         <p className=" text-second-text-color text-sm tracking-[.0125rem] w-[21.9375rem]">
@@ -48,7 +48,7 @@ export default function QualityEducation() {
           of Classical physics: Newtonian mechanics{" "}
         </p>
         <LearnMore />
-      </div> */}
+      </div>
     </div>
   );
 }
